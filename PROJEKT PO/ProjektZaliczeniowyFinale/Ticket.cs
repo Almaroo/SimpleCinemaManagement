59487bf1b2ef67323a56da97f7b8af77ea9b9b7c
Marek Barszcz <51239869+Almaroo@users.noreply.github.com>
2020-02-11 22:37:16 +0100

    Add files via upload

---
+++ PROJEKT PO/ProjektZaliczeniowyFinale/Ticket.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektZaliczeniowyFinale
+{
+    public static class Ticket
+    {
+        public static void GenerateTicket(Showing showing, bool isDiscounted, int row, int seat)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine("=== TICKET ===");
+            stringBuilder.AppendLine($"Type: {showing.GetType().Name} {(showing.IsPremiere ? "Premiere" : null)}");
+            stringBuilder.AppendLine($@"Date: {showing.ShowingDate.ToString("dddd, dd MMMM yyyy HH:mm:ss")}");
+            stringBuilder.AppendLine($"Row: {row}, Seat: {seat}");
+            stringBuilder.AppendLine($"Price: {(isDiscounted ? showing.Price/2 : showing.Price):C}");
+            stringBuilder.AppendLine($"Discount: {(isDiscounted ? "YES" : "NO")}");
+
+            Console.WriteLine(stringBuilder.ToString());
+
+        }
+    }
+}
